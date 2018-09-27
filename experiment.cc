62bec406fbefd0caef1b48b79a82cba006d78da1
riya305 <riya.saxena3012@gmail.com>
2018-09-26 22:27:42 -0700

    Analysis

---
+++ experiment.cc
@@ -0,0 +1,138 @@
+#include <ctime>
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+ 
+/* Function to sort an array using insertion sort*/
+void insertionSort(int arr[], int n)
+{
+   int i, key, j;
+   for (i = 1; i < n; i++)
+   {
+       key = arr[i];
+       j = i-1;
+ 
+       /* Move elements of arr[0..i-1], that are
+          greater than key, to one position ahead
+          of their current position */
+       while (j >= 0 && arr[j] > key)
+       {
+           arr[j+1] = arr[j];
+           j = j-1;
+       }
+       arr[j+1] = key;
+   }
+}
+ 
+// First subarray is arr[l..m] 
+// Second subarray is arr[m+1..r] 
+void merge(int arr[], int l, int m, int r) 
+{ 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 =  r - m; 
+  
+    /* create temp arrays */
+    int L[n1], R[n2]; 
+  
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1+ j]; 
+  
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray 
+    j = 0; // Initial index of second subarray 
+    k = l; // Initial index of merged subarray 
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < n1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < n2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+  
+/* l is for left index and r is right index of the 
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r) 
+{ 
+    if (l < r) 
+    { 
+        // Same as (l+r)/2, but avoids overflow for 
+        // large l and h 
+        int m = l+(r-l)/2; 
+  
+        // Sort first and second halves 
+        mergeSort(arr, l, m); 
+        mergeSort(arr, m+1, r); 
+  
+        merge(arr, l, m, r); 
+    } 
+} 
+
+// A utility function to print an array of size n
+void printArray(int arr[], int n)
+{
+   int i;
+   for (i=0; i < n; i++)
+       printf("%d ", arr[i]);
+   printf("\n");
+}
+ 
+ 
+ 
+/* Driver program to test insertion sort */
+int main()
+{
+	int n=0;
+ 	cin >> n;
+ 	srand( (unsigned) time(NULL) );
+ 	int* arr = new int[n];
+ 	for(int i = 0; i < n; i++){
+		arr[i] = rand() % 100000;
+	}
+	int startI = clock();
+    insertionSort(arr, n);
+    int stopI = clock(); 
+    int durationI = (stopI-startI)/double(CLOCKS_PER_SEC)*1000*1000; 
+    cout << "Time taken by insertionSort: " << durationI << " nanoseconds" << endl;
+    int startM = clock(); 
+    mergeSort(arr,0,n-1);
+    int stopM = clock(); 
+    int durationM = (stopM-startM)/double(CLOCKS_PER_SEC)*1000*1000; 
+    cout << "Time taken by mergeSort: " << durationM << " nanoseconds" << endl; 
+    // printArray(arr, n);
+ 
+    return 0;
+
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+experiment: experiment.cc
+	g++ -std=c++11 -o experiment experiment.cc -I.
